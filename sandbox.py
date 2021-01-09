42b78a5b585fe2319299c6aa82d4022c0c351c51
gkafadarov <georgiprobg5@gmail.com>
2021-01-10 00:03:41 +0200

    Update sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -6,10 +6,16 @@ def list_subscibed_shows():
     scope = "user-library-read user-read-playback-position"
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
     shows = sp.current_user_saved_shows()
+    results = []
     for item in shows['items']:
         show = item['show']
-        print(show['name'])
-
+        result = {
+            'id': show['id'],
+            'name':show['name']
+        }
+        results.append(result)
+    return results
 
 if __name__ == "__main__":
-    list_subscibed_shows()
+    results=list_subscibed_shows()
+    print(results)

33c918ee7b4ea94b61acb0c29f6cc9d0fcccdd36
gkafadarov <georgiprobg5@gmail.com>
2026-01-14 18:59:04 +0200

    Integrating virtualenv variables

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from spotify_utils import get_latest_user_episodes
 from transcript_generator import get_transcripts
+from dotenv import load_dotenv
+
+# Load environment variables from .secrets file
+load_dotenv('.secrets')
+
 
 if __name__ == "__main__":
     user_episodes = get_latest_user_episodes()
